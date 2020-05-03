8611d7aedf0e87b8bf1467ba8b1ac58f6fd9a60a
thang061092 <63143765+thang061092@users.noreply.github.com>
2020-05-03 14:16:33 +0700

    Delete sound.js

--- caseStudy/caseStudy/sound.js
+++
@@ -1,9 +0,0 @@
-class Sound{
-    constructor(fileName) {
-        this.fileName= fileName;
-        this.audio= new Audio('sound/'+this.fileName);
-    }
-    start(){
-        this.audio.play();
-    }
-}
